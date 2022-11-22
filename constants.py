953a824bc548e9c189ca2005764a4934427767e9
patata <patataofcourse@gmail.com>
2022-11-22 16:39:56 +0100

    Initial commit

---
+++ constants.py
@@ -0,0 +1,15 @@
+# change those to fit your bot!
+BOT_NAME = "testbot"
+BOT_VERSION = "no version"
+BOT_PREFIX = "!"
+BOT_ACTIVITY = ""
+BOT_OWNERS = ()
+BOT_HELPERS = ()
+BOT_ERROR_CHANNEL = 0
+BOT_COLOR = 0xffffff
+
+# customize your text!
+BOT_KILL_MESSAGE = "Bot killed!"
+BOT_ERRWIPE_MESSAGE = "Wiped all the errors!"
+BOT_ERROR_MESSAGE = "An error happened! Contact the bot owner for more details."
+BOT_HELP_TITLE = f"{BOT_NAME} help"
