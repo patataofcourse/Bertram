bdc2e762c8c2f02f444a699bd276b44a75552950
patataofcourse <patataofcourse@gmail.com>
2023-01-20 00:58:47 +0100

    Add !luma solve option for effect file too big

--- module/luma.py
+++ module/luma.py
@@ -330,5 +330,12 @@ async def solve(ctx, link = None):
             value = "- __100%__ chance\nSeems like you should get this fixed, and quick!",
             inline = False
         )
+    
+    if dump.pc == 0x001392c4 and dump.exc_type == 2:
+        embed.add_field (
+            name = "Ran out of effect file memory (using Karate Man's effect file)",
+            value = "- __~80%__ chance\nUse Bunny Hop's effect file instead!",
+            inline = False
+        )
 
     await ctx.send(embed = embed)
