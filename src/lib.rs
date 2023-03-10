8252ed3770f7ed099434dd9597b7c7fa9c34075f
patataofcourse <patataofcourse@gmail.com>
2023-03-10 12:44:35 +0100

    Port !ctru functionality to Rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,2 @@
 pub mod crash;
+pub mod ctru;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", bertram::ctru::CtruError::from_code(0xd8c3fbf3));
+    println!("{}", bertram::ctru::CtruError::from_code(0xc8804478))
 }
